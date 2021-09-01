bacba415ebc69f26b8496ee60bb8a561b378243a
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2021-09-01 14:33:12 +0900

    print msg

--- app.py
+++ app.py
@@ -146,9 +146,9 @@ async def get_massages_from_chatbot():
 
     # 넘어온 JSON에서 메세지 받아 임시 리스트에 append
     body = request.get_json()
-    print(body)
     message_to_model = body['userRequest']['utterance']
     message_list.append(message_to_model)
+    print(message_to_model)
 
     # 처음 대화가 시작되는 순간에만 사용하기 위해 count_start 를 바꿔줌
     # 두번째 말풍선부턴 실행되지 않음
